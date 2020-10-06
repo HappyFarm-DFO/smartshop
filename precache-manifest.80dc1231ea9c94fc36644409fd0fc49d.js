b90a8fd071b219c80d202415c707667c07459e90
maxxflyer <maxxflyer@gmail.com>
2020-10-06 14:43:29 +0200

    Add files via upload

---
+++ precache-manifest.80dc1231ea9c94fc36644409fd0fc49d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5051af0df7e56b326ca2d6036ed0d7d",
+    "url": "/smartshop/index.html"
+  },
+  {
+    "revision": "97a670b7726477720a6c",
+    "url": "/smartshop/static/css/main.b2d0b002.chunk.css"
+  },
+  {
+    "revision": "8d444aca31fc4b52f443",
+    "url": "/smartshop/static/js/2.1fbdaaf7.chunk.js"
+  },
+  {
+    "revision": "2c3cabfca321a7c72463e6894e9161ef",
+    "url": "/smartshop/static/js/2.1fbdaaf7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97a670b7726477720a6c",
+    "url": "/smartshop/static/js/main.036e9084.chunk.js"
+  },
+  {
+    "revision": "ce00b0283e727269bc9e",
+    "url": "/smartshop/static/js/runtime-main.031162e3.js"
+  }
+]);
